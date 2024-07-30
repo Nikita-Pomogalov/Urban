ef9642ee3d7f8778906e92ccb4d137ba87d87c2c
Никита Помогалов <nikitapomogalov@MacBook-Pro-Nikita.local>
2024-07-30 16:34:54 +0300

    m_9_7

--- for_me/for_me.py
+++ for_me/for_me.py
@@ -84,6 +84,49 @@
 # ex_str = '4' * 100_100
 # print(f'Размер списка - {sys.getsizeof(ex_str)}')
 
+# def uppercase(func):
+#     def wrapper():
+#         original_result = func()
+#         modified_result = original_result.upper()
+#         return modified_result
+#     return wrapper
+#
+#
+# @uppercase
+# def greet():
+#     return 'Hello!'
+#
+#
+# print(greet())
+
+
+# import time
+# import sys
+#
+# def time_truck(func):
+#     def surrogate(*args, **kwargs):
+#         started_at = time.time()
+#
+#         result = func(*args, **kwargs)
+#
+#         ended_at = time.time()
+#         elapsed = round(ended_at - started_at, 4)
+#         print(f'Функция работала {elapsed} секунд(ы)')
+#         return result
+#     return surrogate
+#
+#
+# @time_truck
+# def digits(*args):
+#     total = 1
+#     for number in args:
+#         total *= number ** 5000
+#     return len(str(total))
+#
+# sys.set_int_max_str_digits(100000)
+#
+# result = digits(3141, 5926, 2718, 2818)
+# print(result)
 
 
 

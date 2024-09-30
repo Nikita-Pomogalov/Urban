bb33f5553e5c7d0e445bbc2e1dfde8539f2b3394
Никита Помогалов <nikitapomogalov@MacBook-Pro-Nikita.local>
2024-09-30 22:55:58 +0300

    16_3

--- module_16_fastAPI/module_16_3.py
+++ module_16_fastAPI/module_16_3.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
 
-from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -12,17 +11,20 @@ async def get_users() -> dict:
     return users
 
 @app.post('/user/{username}/{age}')
-async def put_user(username: str, age: int) -> str:
+async def put_user(username: Annotated[str, Path(min_length=3, max_length=30, description='Enter your Username', example='Bob')],
+                   age: Annotated[int, Path(ge=1, le=100, description='Enter your age')]) -> str:
     new_index = str(int(max(users, key=int)) + 1)
     users[new_index] = f'Имя: {username}, возраст: {age}'
     return f'User {new_index} was registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(user_id: Annotated[str, Path(max_length=10, description='Write id to change')],
+                      username: Annotated[str, Path(min_length=3, max_length=30, description='Enter your Username', example='Bob')],
+                      age:  Annotated[int, Path(ge=1, le=100, description='Enter your age')]) -> str:
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} has been updated'
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
+async def delete_user(user_id: Annotated[str, Path(max_length=20, description='Write id to delete')]) -> str:
     users.pop(user_id)
     return f'The user {user_id} has been deleted'

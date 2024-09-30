c48e6e40ce95dafe090fd854348e26a419522316
Никита Помогалов <nikitapomogalov@MacBook-Pro-Nikita.local>
2024-09-30 22:01:21 +0300

    m_16_2

--- module_16_fastAPI/main.py
+++ module_16_fastAPI/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -6,9 +7,22 @@ app = FastAPI()
 async def welcome() -> dict:
     return {"message": "Hello World"}
 
-@app.get("/main")
-async def welcome() -> dict:
-    return {"message": "Main Page"}
+@app.get("/user/A/B")
+async def news() -> dict:
+    return {"message": f"Hello, Tester"}
+
+@app.get('/id')
+async def id_paginator(username: str = 'alex', age: int = 24) -> dict:
+    return {'User': username, 'Age': age}
+
+@app.get("/user/{username}/{id}")
+async def news(username: Annotated[str, Path(min_length=3, max_length=15, description='Enter username', example='montes')],
+               id: int) -> dict:
+    return {"message": f"Hello, {username} {id}"}
+
+
+
+
 
 # get - адрес строки ?переменная=значение
 # post - формы - оформить заказ в магазине
